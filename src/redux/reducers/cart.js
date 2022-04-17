9fa01913a18acd70fa5bef4d96110c149827bc84
MarkEmdin <emdinmark@gmail.com>
2022-04-17 14:19:36 +0300

    add pizzas in cart, add  empty cart, change data structure in redux

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -3,22 +3,36 @@ const initialState = {
   totalPrice: 0,
   totalCount: 0,
 };
+const gerTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum, 0);
+
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_PIZZA_CART': {
+      const currentPizzasItem = !state.items[action.payload.id]
+        ? [action.payload]
+        : [...state.items[action.payload.id].items, action.payload];
       const newItem = {
         ...state.items,
-        [action.payload.id]: !state.items[action.payload.id]
-          ? [action.payload]
-          : [...state.items[action.payload.id], action.payload],
+        [action.payload.id]: {
+          items: currentPizzasItem,
+          totalPrice: gerTotalPrice(currentPizzasItem),
+        },
       };
-      const allPizas = [].concat.apply([], Object.values(newItem));
-      const allPrice = allPizas.reduce((sum, obj) => obj.price + sum, 0);
+      const items = Object.values(newItem).map((obj) => obj.items);
+      const allPizas = [].concat.apply([], items);
+      const allPrice = gerTotalPrice(allPizas);
       return {
         ...state,
         items: newItem,
         totalCount: allPizas.length,
         totalPrice: allPrice,
+      };
+    }
+    case 'CLEAR_CART': {
+      return {
+        items: {},
+        totalPrice: 0,
+        totalCount: 0,
       };
     }
     default:
